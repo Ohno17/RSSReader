05138e53aa7e46c7e8412c821fbf95c9e69a04b1
Ohno17 <ajaylego30@gmail.com>
2026-01-29 20:15:25 -0600

    meson build

--- script.sh
+++
@@ -1,8 +0,0 @@
-# For the toolchain
-sudo apt-get install build-essential autoconf automake bison flex gawk libtool libtool-bin libncurses-dev curl file git gperf help2man texinfo unzip wget
-
-# For the sdk
-sudo apt-get install curl sed libarchive-dev nettle-dev
-
-# For this tutorial
-sudo apt-get install meson gtk2.0 libgtk2.0-dev
